15b0ef7cf398e93638171d6cee857bf05abcd1f9
Kenz8489 <stark858981@gmail.com>
2025-02-16 20:49:50 +0530

    Updated features and fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Hero from './components/Hero';
 import About from './components/About';
@@ -7,9 +7,29 @@ import Contact from './components/Contact';
 import Footer from './components/Footer';
 import SplashCursor from './components/SplashCursor';
 import Events from './components/Events';
-
+import Lenis from "@studio-freight/lenis";
 
 const App = () => {
+
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.4, // Adjust speed
+      smooth: true,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // Smoother easing
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy(); // Clean up when unmounting
+    };
+  }, []);
+
   return (
     <Router>
       <main className='relative min-h-screen w-screen overflow-x-hidden'>

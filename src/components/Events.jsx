2d2f07b94585f01dea76a6ada5a8e6fe79dfa9df
Kenz8489 <stark858981@gmail.com>
2025-02-16 17:55:41 +0530

    Updated features and fixed bugs

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -2,12 +2,47 @@ import React, { useEffect } from 'react'
 // import ShapeBlur from './ui/ShapeBlur'
 import GridDistortion from './ui/GridDistortion'
 import TiltedCard from './ui/TiltedCard/TiltedCard';
+import TrueFocus from './ui/TrueFocus/TrueFocus';
+import InfiniteMenu from './ui/InfiniteMenu/InfiniteMenu'
 
 const Events = () => {
 
-  useEffect(() => {
-    window.scrollTo(0, 0); // Scroll to the top
-  }, []);
+  // useEffect(() => {
+  //   window.scrollTo(0, 0); // Scroll to the top
+  // }, []);
+
+  const items = [
+    {
+      image: '/img/p_1.png',
+      link: 'https://www.instagram.com/pistonia_mcet/',
+      description: 'Pretty cool, right?'
+    },
+    {
+      image: '/img/p_2.png',
+      link: 'https://www.instagram.com/pistonia_mcet/',
+      description: 'there is no going back.'
+    },
+    {
+      image: '/img/p_3.png',
+      link: 'https://www.instagram.com/pistonia_mcet/',
+      description: 'Roads are meant to be conquered.'
+    },
+    {
+      image: '/img/p_4.png',
+      link: 'https://www.instagram.com/pistonia_mcet//',
+      description: 'A full tank and open roads—pure bliss.'
+    },
+    {
+      image: '/img/p_5.png',
+      link: 'https://www.instagram.com/pistonia_mcet/',
+      description: 'Every car has a story to tell.'
+    },
+    {
+      image: '/img/p_6.png',
+      link: 'https://www.instagram.com/pistonia_mcet/',
+      description: 'Drive it, love it, live it.'
+    }
+  ];
 
   return (
     <div className="relative w-full min-h-screen">
@@ -27,16 +62,15 @@ const Events = () => {
       </div>
 
       {/* Pro Show Section */}
-      <div className="flex flex-col md:flex-row justify-around items-center gap-4 px-6 py-12">
-
+      <div className="flex flex-col md:flex-row justify-around items-center bg-black gap-4 px-16 py-12 ">
         <TiltedCard
           imageSrc="/img/8m_3.png"
           altText="8EEN MUSIC"
           captionText="8EEN MUSIC"
-          containerHeight="500px"
-          containerWidth="500px"
-          imageHeight="500px"
-          imageWidth="500px"
+          containerHeight="450px"
+          containerWidth="450px"
+          imageHeight="450px"
+          imageWidth="450px"
           rotateAmplitude={12}
           scaleOnHover={1.1}
           showMobileWarning={false}
@@ -48,10 +82,10 @@ const Events = () => {
           imageSrc="/img/tc_3.png"
           altText="THAMARASSERY CHURAM"
           captionText="THAMARASSERY CHURAM"
-          containerHeight="500px"
-          containerWidth="500px"
-          imageHeight="500px"
-          imageWidth="500px"
+          containerHeight="450px"
+          containerWidth="450px"
+          imageHeight="450px"
+          imageWidth="450px"
           rotateAmplitude={12}
           scaleOnHover={1.1}
           showMobileWarning={false}
@@ -61,8 +95,23 @@ const Events = () => {
       </div>
 
       {/* Auto Show Section */}
-      <div className="h-screen flex items-center justify-center bg-black text-white text-5xl font-bold">
-        Pistonia '25
+      <div className="h-screen flex flex-col items-center justify-center bg-black text-white text-5xl font-bold">
+
+        <div className=' '>
+          <TrueFocus
+            sentence="Pistonia '25"
+            manualMode={false}
+            blurAmount={9}
+            borderColor="purple"
+            animationDuration={0.6}
+            pauseBetweenAnimations={1}
+          />
+        </div>
+
+        <div style={{ height: '500px', position: 'relative' }} className='p-5'>
+          <InfiniteMenu className='rounded-lg' items={items} />
+        </div>
+
       </div>
 
       {/* Main Events */}
--- src/components/ui/TiltedCard/TiltedCard.jsx
+++ src/components/ui/TiltedCard/TiltedCard.jsx
@@ -74,7 +74,7 @@ export default function TiltedCard({
   return (
     <figure
       ref={ref}
-      className="relative w-full h-full [perspective:800px] flex flex-col items-center justify-center"
+      className="relative w-full h-full [perspective:800px] flex flex-col items-center justify-center "
       style={{
         height: containerHeight,
         width: containerWidth,
